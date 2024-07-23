4816067b3fdd0c56f33c526d52772e54499fef1f
jishu91 <120646662+jishu91@users.noreply.github.com>
2024-07-23 23:05:56 +0530

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.getElementById("count-el").innerHTML=5
+
+let firstbatch = 5
+let secondbatch = 7
+
+let count = firstbatch + secondbatch
+
+console.log(count)
+
+// 1. Create a variable, myAge, and set its value to your age
+// let myAge = 24
+// 2. Log the myAge variable to the console
+// console.log(myAge)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 24
+let humanDogRatio = 4
+// 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console
+console.log(myDogAge)
